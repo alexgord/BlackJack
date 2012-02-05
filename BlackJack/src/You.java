c13a317a0c96e565de8986caf2dd5159a7ff9fc8
Alexandre <alesimar@gmail.com>
2012-02-04 23:22:32 -0500

    fixed: ace splitting if

--- BlackJack/src/You.java
+++ BlackJack/src/You.java
@@ -17,7 +17,7 @@ public class You extends Player
 		
 		for ( handWillPlay = 0; handWillPlay < hand.size(); handWillPlay++ )
 		{
-			if ( hand.get(handWillPlay).getHand().get(0).getFace() != Rank.ACE || handWillPlay == 0 )
+			if ( !(hand.get(handWillPlay).getHand().get(0).getFace() == Rank.ACE && handWillPlay > 0) )
 			{
 				String options = GenerateOptions(r);
 				
@@ -55,6 +55,8 @@ public class You extends Player
 			}
 			else
 			{
+				System.out.println(hand.get(handWillPlay).toString());
+				System.out.println("Hand number: " + (handWillPlay + 1));
 				System.out.println("Hand was skipped because it had an ace when it was split");
 			}
 		}
